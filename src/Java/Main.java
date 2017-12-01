88e6c10182737ab55c73c5fbf9f7e96f43656e21
User <User@Admin>
2017-12-01 20:07:39 +0800

    sprint 2

---
+++ src/Java/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+
+import Domain.AddFunction;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<AddFunction> add = new ArrayList<>();
+    int i;
+    int c;
+    String input;
+    int id = 1000;
+    
+    
+    public void AddDetails(){
+        add.add(new AddFunction("",id,"","","","",""));
+        add.add(new AddFunction("",id,"","","","",""));
+        add.add(new AddFunction("",id,"","","","",""));
+        add.add(new AddFunction("",id,"","","","",""));
+    }
+        
+        
+    }
